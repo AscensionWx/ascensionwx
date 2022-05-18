3a65eec63df58aae934cf3a60d9d02754f28b132
sunburntcat <32531379+sunburntcat@users.noreply.github.com>
2022-05-17 20:54:31 -0500

    Update and rename prepare_915mhz.bat to copy_lmic_config_example.bat

---
+++ TTGO T-beam firmware/0.3.0/copy_lmic_config_example.bat
@@ -0,0 +1,4 @@
+rem Sets sim link to correct lmic frequency config file
+
+rem Need to copy lmic config file to arduino lmic library
+copy /y lmic_project_config.h "C:\Users\nick_\Documents\Arduino_Beta\libraries\arduino-lmic\project_config\lmic_project_config.h"
--- TTGO T-beam firmware/0.3.0/prepare_915mhz.bat
+++
@@ -1,4 +0,0 @@
-rem Sets sim link to correct lmic frequency config file
-
-rem Need to copy lmic config file to arduino lmic library
-copy /y lmic_configs\915.h "C:\Users\nick_\Documents\Arduino_Beta\libraries\arduino-lmic\project_config\lmic_project_config.h"
